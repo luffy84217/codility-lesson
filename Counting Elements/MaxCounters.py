6fc34faaab7b85dfedd1b5185108477b72e3be23
luffy84217 <luffy84217@gmail.com>
2020-01-19 19:50:07 +0800

    perf: improve performance in large_random2 test

--- Counting Elements/MaxCounters.py
+++ Counting Elements/MaxCounters.py
@@ -1,21 +1,25 @@
 def solution(N, A):
-    result = [0] * N
-    count = [0] * N
+    result = [None]*N
+    final_floor = 0
     curr_max = 0
-    acc_max = 0
+
+    for counterNum in A:
+        if counterNum == N+1:
+            final_floor = curr_max
+            result = [None]*N
+        else:
+            if result[counterNum-1] == None:
+                result[counterNum-1] = 1
+            else:
+                result[counterNum-1] += 1
+            if curr_max < final_floor+result[counterNum-1]:
+                curr_max = final_floor+result[counterNum-1]
     
-    for num in A:
-        if num == N + 1:
-            acc_max += curr_max
-            count = [0] * N
-            curr_max = 0
-            continue
-        count[num - 1] += 1
-        curr_max = max(count)
-
-    for i in range(len(result)):
-        result[i] = acc_max + count[i]
-
+    for i in range(N):
+        if result[i] == None:
+            result[i] = 0
+        result[i] += final_floor
+    
     return result
     
     # def solution(N, A):
